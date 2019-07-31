9011ed8daff6b1418b6d4fbd91772706851d2cd1
Bartoszkie <bartosz.slysz23@gmail.com>
2019-07-31 18:13:04 +0200

    updating our cart components and menu items

--- src/App.js
+++ src/App.js
@@ -2,22 +2,18 @@ import React from "react";
 //Route takes 3 paramethers: exact path component
 import { Route, Switch } from "react-router-dom";
 
-import HomePage from "./pages/homepage/homepage.component";
-import "./App.css";
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import "./App.css";
 
 function App() {
   return (
     <div>
     {/* Jeśli route będący w switchu pasujący URL, nie wyrenderuje nicz innego jak tego Routa */}
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );

aff1db9e4c1b248eaf4239bc8b89db6660986983
Bartoszkie <bartosz.slysz23@gmail.com>
2019-08-23 19:01:31 +0200

    added mapStateToProps in App.js, created proper display of SignIn component, created user.types.js to display proper values of types that goes to reducers

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 //Route takes 3 paramethers: exact path component
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
-import {connect} from 'react-redux';
-import {setCurrentUser} from './redux/user/user.actions';
+import { connect } from "react-redux";
+import { setCurrentUser } from "./redux/user/user.actions";
 
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.components';
-import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
-import {auth, createUserProfileDocument} from './firebase/firebase.utils';
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.components";
+import SignInSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 import "./App.css";
 
@@ -22,51 +22,70 @@ class App extends React.Component {
   //   };
   // }
 
-  unsubscribeFromAuth = null
+  unsubscribeFromAuth = null;
 
   componentDidMount() {
-
-    const {setCurrentUser} = this.props;
-
+    const { setCurrentUser } = this.props;
+    
+    
     //methoda z firebase.auth
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAth => {
-      if(userAth) {
+      if (userAth) {
         const userRef = await createUserProfileDocument(userAth);
-        
+
         userRef.onSnapshot(snapShot => {
           setCurrentUser({
-              id: snapShot.id, 
-              ...snapShot.data()
+            id: snapShot.id,
+            ...snapShot.data()
           });
         });
-      }else{
+      } else {
         setCurrentUser(userAth);
       }
     });
   }
 
   componentWillUnmount() {
-    console.log('component unmounted');
+    console.log("component unmounted");
+
     this.unsubscribeFromAuth();
   }
 
-  render(){
+  render() {
     return (
       <div>
-      <Header/>
-      {/* Jeśli route będący w switchu pasujący URL, nie wyrenderuje nicz innego jak tego Routa */}
+        <Header />
+        {/* Jeśli route będący w switchu pasujący URL, nie wyrenderuje nicz innego jak tego Routa */}
         <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInSignUpPage} />
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to="/" />
+              ) : (
+                <SignInSignUpPage />
+              )
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
 
+//chcemy miec dostep do currentUser w naszych propsach więc musimy zrobić mapStateToProps
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
-})
+});
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);

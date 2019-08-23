42c3b6d4181164bc27811f998e5755655bd4ac63
Bartoszkie <bartosz.slysz23@gmail.com>
2019-08-24 00:04:06 +0200

    sooooo, added cart.utils.js where we will write our methods that goes to our reducers - to keep simplicity of our code. Added addItemToCart function with quantity property

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,9 @@
 import CartActionTypes from './cart.types';
+import {addItemToCart}  from './cart.utils';
 
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true, 
+    cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -10,6 +12,14 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         return {
             ...state, 
             hidden: !state.hidden
+        }
+        case CartActionTypes.ADD_ITEM: 
+        return {
+            ...state, 
+            //w cartItems mają być stare elementy oraz nowe dodane na sam koniec
+            //w tym celu spreadujemy startą tablicę oraz dodajemy nowe elementy zwórcone przez acton.payload
+            // cartItems: [...state.cartItems, action.payload] 
+            cartItems: addItemToCart(state.cartItems, action.payload)
         }
         default: 
             return state

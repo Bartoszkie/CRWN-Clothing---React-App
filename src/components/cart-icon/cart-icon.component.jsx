8ea6952d9002116e08d7400d32b773c03d08d830
Bartoszkie <bartosz.slysz23@gmail.com>
2019-08-24 16:59:41 +0200

    sooo, added cart.selector which is waaaay more easier to understand, reusable and performacier (xd). Now method mapStateToProps will not re-render every time state changes.

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,21 +5,31 @@ import { connect } from "react-redux";
 import "./cart-icon.styles.scss";
 
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
-
+import {selectCartItemsCount} from '../../redux/cart/cart.selector';
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, itemCount}) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
+
+// const mapStateToProps = ({cart: {cartItems}}) => ({
+//   itemCount: cartItems.reduce(
+//     (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0
+//   )
+// });
+
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state)
+});
 
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(CartIcon);
